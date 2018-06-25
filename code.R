763bd30cbd9107974a4efbbdcab0045f6e32b09f
GPICCOL <gpiccoli@cct.lsu.edu>
2018-06-25 18:24:23 +0200

    added total of all exams

--- code.R
+++ code.R
@@ -1,43 +1,213 @@
 library(abind)
-a <- read.csv("data/anna.csv", stringsAsFactors = FALSE)
-g <- read.csv("data/grace.csv", stringsAsFactors = FALSE)
-r <- read.csv("data/rachel.csv", stringsAsFactors = FALSE)
-ag <- read.csv("data/autog.csv", stringsAsFactors = FALSE)
+a1 <- read.csv("data/anna_exam1.csv", stringsAsFactors = FALSE)#exam1
+g1 <- read.csv("data/grace_exam1.csv", stringsAsFactors = FALSE)
+r1 <- read.csv("data/rachel_exam1.csv", stringsAsFactors = FALSE)
+ag1 <- read.csv("data/autog_exam1.csv", stringsAsFactors = FALSE)
+a2 <- read.csv("data/anna_exam2.csv", stringsAsFactors = FALSE)#exam2
+g2 <- read.csv("data/grace_exam2.csv", stringsAsFactors = FALSE)
+r2 <- read.csv("data/rachel_exam2.csv", stringsAsFactors = FALSE)
+ag2 <- read.csv("data/autog_exam2.csv", stringsAsFactors = FALSE)
+a3 <- read.csv("data/anna_exam3.csv", stringsAsFactors = FALSE)#exam3
+g3 <- read.csv("data/grace_exam3.csv", stringsAsFactors = FALSE)
+r3 <- read.csv("data/rachel_exam3.csv", stringsAsFactors = FALSE)
+ag3 <- read.csv("data/autog_exam3.csv", stringsAsFactors = FALSE)
+t1 <- read.csv("data/truth_exam1.csv", stringsAsFactors= FALSE)#truth exam 1 
+t2 <- read.csv("data/truth_exam2.csv", stringsAsFactors= FALSE)#truth exam 2 
+t3 <- read.csv("data/truth_exam3.csv", stringsAsFactors= FALSE)#truth exam 3 
 
-colnames(a) <- tolower(colnames(a))
-colnames(g) <- tolower(colnames(g))
-colnames(r) <- tolower(colnames(r))
-colnames(ag) <- tolower(colnames(ag))
 
-identical(colnames(a), colnames(g))
-identical(ag[1], g[1])
 
-col_retain <- intersect(colnames(a), colnames(ag))
-a <- a[3:52, col_retain]
-g <- g[3:52, col_retain]
-r <- r[3:52, col_retain]
+colnames(a1) <- tolower(colnames(a1))
+colnames(g1) <- tolower(colnames(g1))
+colnames(r1) <- tolower(colnames(r1))
+colnames(ag1) <- tolower(colnames(ag1))
+colnames(t1) <- tolower(colnames(t1))
 
-identical(colnames(a), colnames(ag))
-colnames(ag)
-ag <- ag[3:52, col_retain]
-identical(colnames(a), colnames(ag))
-colnames(ag)
+identical(colnames(a3), colnames(ag3))
+identical(ag1[1], g1[1])
+############Exam 1######################
+col_retain <- intersect(colnames(a1), colnames(ag1))
+a1 <- a1[2:28, col_retain]
+g1 <- g1[2:28, col_retain]
+r1 <- r1[2:28, col_retain]
 
-aa <- abind(as.matrix(a[, c(4:29)]), as.matrix(g[, c(4:29)]), as.matrix(r[, c(4:29)]), as.matrix(ag[, c(4:29)]), 
-            along = 3, new.names = list(unname(unlist(a[1])), NULL, c("anna", "grace", "rachel", "autograder")))
+identical(colnames(a1), colnames(ag1))
+colnames(ag1)
+ag1 <- ag1[2:28, col_retain]
+identical(colnames(a1), colnames(ag1))
+colnames(ag1)
+identical(colnames(a1), colnames(t1))
+colnames(t1)
+t1 <- t1[2:28, col_retain]
+identical(colnames(g1), colnames(t1))
+colnames(t1)
 
-totals <- colSums(aa, dims = 1)
-avg <- round(rowMeans(aa, dims = 2), 2)
-stdv <- round(apply(aa, c(1,2), sd), 2)
 
-perc_correct <- sum(stdv==0)/(dim(stdv)[1]*dim(stdv)[2])
+aa1 <- abind(as.matrix(a1[, c(3:9)]), as.matrix(g1[, c(3:9)]), as.matrix(r1[, c(3:9)]), as.matrix(ag1[, c(3:9)]), 
+             along = 3, new.names = list(unname(unlist(a1[1])), NULL, c("anna", "grace", "rachel", "autograder")))#####for rater agreement
 
-rowSums(aa, dims = 2)
+totals1 <- colSums(aa1, dims = 1)
+avg1 <- round(rowMeans(aa1, dims = 2), 2)
+stdv1 <- round(apply(aa1, c(1,2), sd), 2)
 
-aa[c("6.2"), c("cmcgre7","wmulli8"), ]
-aa[c("7","9.a"), c("cmcgre7","smanto3"), ]
+perc_correct1 <- sum(stdv1==0)/(dim(stdv1)[1]*dim(stdv1)[2])
 
-#write.csv(totals, file = "totals.csv")
-#write.csv(avg, file = "avg.csv")
-#write.csv(stdv, file = "stdv.csv")
+rowSums(aa1, dims = 2)
+# write.csv(totals1, file = "totals1.csv")
+# write.csv(avg1, file = "avg1.csv")
+# write.csv(stdv1, file = "stdv1.csv")
+
+########automatic grading vs truth
+agt1 <- abind(as.matrix(ag1[, c(3:9)]), as.matrix(t1[, c(3:9)]), as.matrix(t1[, c(3:9)]),
+              along = 3, new.names = list(unname(unlist(ag1[1])), NULL, c("autograder", "truth", "t1")))
+
+agt1sub<- asub(agt1, list(1:27,1:7, 1:2))
+totalsAGT1 <- colSums(agt1sub, dims = 1)
+avgAGT1 <- round(rowMeans(agt1sub, dims = 2), 2)
+stdvAGT1 <- round(apply(agt1sub, c(1,2), sd), 2)
+
+perc_correctT1ag <- sum(stdvAGT1==0)/(dim(stdvAGT1)[1]*dim(stdvAGT1)[2])
+
+rowSums(agt1sub, dims = 2)
+######## humans vs truth
+aht1 <- abind(as.matrix(a1[, c(3:9)]), as.matrix(g1[, c(3:9)]), as.matrix(r1[, c(3:9)]), as.matrix(t1[, c(3:9)]), 
+             along = 3, new.names = list(unname(unlist(a1[1])), NULL, c("anna", "grace", "rachel", "truth")))
+
+totalsT1 <- colSums(aht1, dims = 1)
+avgT1 <- round(rowMeans(aht1, dims = 2), 2)
+stdvT1 <- round(apply(aht1, c(1,2), sd), 2)
+
+perc_correctT1hum <- sum(stdvT1==0)/(dim(stdvT1)[1]*dim(stdvT1)[2])
+
+rowSums(aht1, dims = 2)
+
+
+
+
+
+
+
+
+############Exam 2######################
+col_retain <- intersect(colnames(a2), colnames(ag2))
+a2 <- a2[2:27, col_retain]
+g2 <- g2[2:27, col_retain]
+r2 <- r2[2:27, col_retain]
+
+identical(colnames(a2), colnames(ag2))
+colnames(ag2)
+ag2 <- ag2[2:27, col_retain]
+identical(colnames(a2), colnames(ag2))
+colnames(ag2)
+t2 <- t2[2:27, col_retain]
+identical(colnames(g2), colnames(t2))
+colnames(t2)
+
+aa2 <- abind(as.matrix(a2[, c(2:8)]), as.matrix(g2[, c(2:8)]), as.matrix(r2[, c(2:8)]), as.matrix(ag2[, c(2:8)]), 
+            along = 3, new.names = list(unname(unlist(a2[1])), NULL, c("anna", "grace", "rachel", "autograder")))
+
+totals2 <- colSums(aa2, dims = 1)
+avg2 <- round(rowMeans(aa2, dims = 2), 2)
+stdv2 <- round(apply(aa2, c(1,2), sd), 2)
+
+perc_correct2 <- sum(stdv2==0)/(dim(stdv2)[1]*dim(stdv2)[2])
+
+rowSums(aa2, dims = 2)
+# write.csv(totals2, file = "totals2.csv")
+# write.csv(avg2, file = "avg2.csv")
+# write.csv(stdv2, file = "stdv2.csv")
+
+
+########automatic grading vs truth
+agt2 <- abind(as.matrix(ag2[, c(2:8)]), as.matrix(t2[, c(2:8)]), as.matrix(t2[, c(2:8)]),
+              along = 3, new.names = list(unname(unlist(ag2[1])), NULL, c("autograder", "truth", "t2")))
+
+agt2sub<- asub(agt2, list(1:26,1:7, 1:2))
+totalsAGT2 <- colSums(agt2sub, dims = 1)
+avgAGT2 <- round(rowMeans(agt2sub, dims = 2), 2)
+stdvAGT2 <- round(apply(agt2sub, c(1,2), sd), 2)
+
+perc_correctT2ag <- sum(stdvAGT2==0)/(dim(stdvAGT2)[1]*dim(stdvAGT2)[2])
+
+rowSums(agt2sub, dims = 2)
+######## humans vs truth
+aht2 <- abind(as.matrix(a2[, c(2:8)]), as.matrix(g2[, c(2:8)]), as.matrix(r2[, c(2:8)]), as.matrix(t2[, c(2:8)]), 
+              along = 3, new.names = list(unname(unlist(a2[1])), NULL, c("anna", "grace", "rachel", "truth")))
+
+totalsT2 <- colSums(aht2, dims = 1)
+avgT2 <- round(rowMeans(aht2, dims = 2), 2)
+stdvT2 <- round(apply(aht2, c(1,2), sd), 2)
+
+perc_correctT2hum <- sum(stdvT2==0)/(dim(stdvT2)[1]*dim(stdvT2)[2])
+
+rowSums(aht2, dims = 2)
+
+
+
+
+
+
+
+
+############Exam 3######################
+col_retain <- intersect(colnames(a3), colnames(ag3))
+a3 <- a3[2:23, col_retain]
+g3 <- g3[2:23, col_retain]
+r3 <- r3[2:23, col_retain]
+
+identical(colnames(a3), colnames(ag3))
+colnames(ag3)
+ag3 <- ag3[2:23, col_retain]
+identical(colnames(a3), colnames(ag3))
+colnames(ag3)
+t3 <- t3[2:23, col_retain]
+identical(colnames(g3), colnames(t3))
+colnames(t3)
+aa3 <- abind(as.matrix(a3[, c(3:10)]), as.matrix(g3[, c(3:10)]), as.matrix(r3[, c(3:10)]), as.matrix(ag3[, c(3:10)]), 
+             along = 3, new.names = list(unname(unlist(a3[1])), NULL, c("anna", "grace", "rachel", "autograder")))
+
+totals3 <- colSums(aa3, dims = 1)
+avg3 <- round(rowMeans(aa3, dims = 2), 2)
+stdv3 <- round(apply(aa3, c(1,2), sd), 2)
+
+perc_correct3 <- sum(stdv3==0)/(dim(stdv3)[1]*dim(stdv3)[2])
+
+rowSums(aa3, dims = 2)
+# write.csv(totals3, file = "totals3.csv")
+# write.csv(avg3, file = "avg3.csv")
+# write.csv(stdv3, file = "stdv3.csv")
+
+
+########automatic grading vs truth 
+agt3 <- abind(as.matrix(ag3[, c(3:10)]), as.matrix(t3[, c(3:10)]), as.matrix(t3[, c(3:10)]),
+              along = 3, new.names = list(unname(unlist(ag3[1])), NULL, c("autograder", "truth", "t3")))
+
+agt3sub<- asub(agt3, list(1:22,1:7, 1:2))
+totalsAGT3 <- colSums(agt3sub, dims = 1)
+avgAGT3 <- round(rowMeans(agt3sub, dims = 2), 2)
+stdvAGT3 <- round(apply(agt3sub, c(1,2), sd), 2)
+
+perc_correctT3ag <- sum(stdvAGT3==0)/(dim(stdvAGT3)[1]*dim(stdvAGT3)[2])
+
+rowSums(agt3sub, dims = 2)
+######## humans vs truth
+aht3 <- abind(as.matrix(a3[, c(3:10)]), as.matrix(g3[, c(3:10)]), as.matrix(r3[, c(3:10)]), as.matrix(t3[, c(3:10)]), 
+              along = 3, new.names = list(unname(unlist(a3[1])), NULL, c("anna", "grace", "rachel", "truth")))
+
+totalsT3 <- colSums(aht3, dims = 1)
+avgT3 <- round(rowMeans(aht3, dims = 2), 2)
+stdvT3 <- round(apply(aht3, c(1,2), sd), 2)
+
+perc_correctT3hum <- sum(stdvT3==0)/(dim(stdvT3)[1]*dim(stdvT3)[2])
+
+rowSums(aht3, dims = 2)
+
+
+
+
+
+
+
+
+
 
---
+++ code_gabriele.R
@@ -0,0 +1,328 @@
+## ----include=FALSE-------------------------------------------------------
+library(abind)
+library(tidyverse)
+library(knitr)
+
+tm <- read.csv("data/time.csv", stringsAsFactors = TRUE, na.strings = 0)
+a <- read.csv("data/anna_exam1.csv", stringsAsFactors = FALSE)#exam1
+g <- read.csv("data/grace_exam1.csv", stringsAsFactors = FALSE)
+r <- read.csv("data/rachel_exam1.csv", stringsAsFactors = FALSE)
+ag <- read.csv("data/autog_exam1.csv", stringsAsFactors = FALSE)
+t <- read.csv("data/truth_exam1.csv", stringsAsFactors= FALSE)#truth exam 1 
+names(t)[1] <- "task"
+t_scores <- read.csv("data/truth_exam1.csv", stringsAsFactors = FALSE)
+names(t_scores)[1] <- "task"
+
+
+# df <- read.csv("data/chunks.csv", stringsAsFactors = TRUE)
+# roster <- read.csv("data/roster.csv", stringsAsFactors = FALSE)
+# ct <- read.csv("data/chunk_tasks.csv", stringsAsFactors = FALSE)
+
+
+## ----reliability_validity, include=FALSE---------------------------------
+colnames(a) <- tolower(colnames(a))
+colnames(g) <- tolower(colnames(g))
+colnames(r) <- tolower(colnames(r))
+colnames(ag) <- tolower(colnames(ag))
+colnames(t) <- tolower(colnames(t))
+colnames(t_scores) <- tolower(colnames(t_scores))
+
+
+identical(colnames(a), colnames(g))
+identical(ag[1], g[1])
+
+
+col_retain <- intersect(colnames(a), colnames(ag))
+a <- a[2:28, col_retain]
+g <- g[2:28, col_retain]
+r <- r[2:28, col_retain]
+
+identical(colnames(a), colnames(ag))
+colnames(ag)
+ag <- ag[2:28, col_retain]
+identical(colnames(a), colnames(ag))
+colnames(ag)
+
+identical(colnames(a), colnames(t))
+identical(ag[1], t[1])
+t <- t[2:28, col_retain]
+identical(colnames(a), colnames(t))
+
+identical(colnames(a), colnames(t_scores))
+identical(ag[1], t_scores[1])
+t_scores <- t_scores[2:28, col_retain]
+identical(colnames(a), colnames(t_scores))
+
+aa <- abind(as.matrix(a[, c(3:9)]), as.matrix(g[, c(3:9)]), as.matrix(r[, c(3:9)]),
+            as.matrix(ag[, c(3:9)]), as.matrix(t[, c(3:9)]), abs(as.matrix(t[, c(3:9)])),
+            as.matrix(t_scores[, c(3:9)]),
+            along = 3, new.names = list(unname(unlist(a[1])), NULL, c("anna", "grace", "rachel", "autograder",
+                                                                      "truth", "positive_truth", "true_scores")))
+
+perc_error <- NULL
+perc_error["autograder"] <- round(sum(aa[ , , 6])/(dim(aa[ , , 6])[1]*dim(aa[ , , 6])[2])*100, 2)
+
+#perfect <- round(apply(aa[1:50, 1:26, c(4,6)], c(1,2), prod), 2)
+#write.csv(perfect, file = "exact_scores.csv")
+
+raters_err <- abind(apply(aa[, , c(1,7)], c(1,2), diff), apply(aa[, , c(2,7)], c(1,2), diff), 
+                    apply(aa[, , c(3,7)], c(1,2), diff), apply(aa[, , c(4,7)], c(1,2), diff),
+                    along = 3, new.names = list(unname(unlist(a[1])), NULL, c("anna", "grace", "rachel", "autograder")))
+
+
+
+points_error <- NULL
+perc_error["anna"] <- round(sum(raters_err[ , , c("anna")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["anna"] <- sum(abs(raters_err[ , , c("anna")]))
+perc_error["grace"] <- round(sum(raters_err[ , , c("grace")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["grace"] <- sum(abs(raters_err[ , , c("grace")]))
+perc_error["rachel"] <- round(sum(raters_err[ , , c("rachel")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["rachel"] <- sum(abs(raters_err[ , , c("rachel")]))
+perc_error["autograder"] <- round(sum(raters_err[ , , c("autograder")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["autograder"] <- sum(abs(raters_err[ , , c("autograder")]))
+
+tot_perc_error <- NULL
+tot_points_error <- NULL
+
+tot_perc_error["anna"] <- sum(raters_err[ , , c("anna")] == 0)
+tot_points_error["anna"] <- sum(abs(raters_err[ , , c("anna")]))
+tot_perc_error["grace"] <- sum(raters_err[ , , c("grace")] == 0)
+tot_points_error["grace"] <- sum(abs(raters_err[ , , c("grace")]))
+tot_perc_error["rachel"] <- sum(raters_err[ , , c("rachel")] == 0)
+tot_points_error["rachel"] <- sum(abs(raters_err[ , , c("rachel")]))
+tot_perc_error["autograder"] <- sum(raters_err[ , , c("autograder")] == 0)
+tot_points_error["autograder"] <- sum(abs(raters_err[ , , c("autograder")]))
+
+tot_dim <- dim(raters_err)[1]*dim(raters_err)[2]
+
+## ------------------------------------------------------------------------
+# str(ct)
+# ct$exercise <- substring(ct$Practice.Assignemnt, 1, 2)
+# mean(ct$Number.of.tasks)
+# range(ct$Number.of.tasks)
+# with(ct, mean(table(exercise)))
+# tot_chunks <- with(ct, tapply(Number.of.tasks, exercise, length))
+
+## ----time----------------------------------------------------------------
+str(tm)
+sapply(tm[ , 2:4], mean, na.rm= TRUE)
+mn <- apply(tm[ , 2:4], 1, mean, na.rm = TRUE)
+names(mn) <- tm[, 1]
+mn
+mean(mn, na.rm = TRUE)
+
+## ----percentage error----------------------------------------------------
+kable(perc_error, caption = "Percentage correct")
+
+## ----points error--------------------------------------------------------
+kable(points_error, caption = "Total score error")
+
+kable(points_error/(sum(ag$points)*dim(raters_err)[2])*100, caption = "Percentage score error")
+
+
+## ----include=FALSE-------------------------------------------------------
+
+a <- read.csv("data/anna_exam2.csv", stringsAsFactors = FALSE)#exam1
+g <- read.csv("data/grace_exam2.csv", stringsAsFactors = FALSE)
+r <- read.csv("data/rachel_exam2.csv", stringsAsFactors = FALSE)
+ag <- read.csv("data/autog_exam2.csv", stringsAsFactors = FALSE)
+t <- read.csv("data/truth_exam2.csv", stringsAsFactors= FALSE)#truth exam 1 
+names(t)[1] <- "task"
+t_scores <- read.csv("data/truth_exam2.csv", stringsAsFactors = FALSE)
+names(t_scores)[1] <- "task"
+
+
+## ----reliability_validity 2, include=FALSE-------------------------------
+colnames(a) <- tolower(colnames(a))
+colnames(g) <- tolower(colnames(g))
+colnames(r) <- tolower(colnames(r))
+colnames(ag) <- tolower(colnames(ag))
+colnames(t) <- tolower(colnames(t))
+colnames(t_scores) <- tolower(colnames(t_scores))
+
+
+identical(colnames(a), colnames(g))
+identical(ag[1], g[1])
+
+
+col_retain <- intersect(colnames(a), colnames(ag))
+a <- a[2:27, col_retain]
+g <- g[2:27, col_retain]
+r <- r[2:27, col_retain]
+
+identical(colnames(a), colnames(ag))
+colnames(ag)
+ag <- ag[2:27, col_retain]
+identical(colnames(a), colnames(ag))
+colnames(ag)
+
+identical(colnames(a), colnames(t))
+identical(ag[1], t[1])
+t <- t[2:27, col_retain]
+identical(colnames(a), colnames(t))
+
+identical(colnames(a), colnames(t_scores))
+identical(ag[1], t_scores[1])
+t_scores <- t_scores[2:27, col_retain]
+identical(colnames(a), colnames(t_scores))
+
+aa <- abind(as.matrix(a[, c(2:8)]), as.matrix(g[, c(2:8)]), as.matrix(r[, c(2:8)]),
+            as.matrix(ag[, c(2:8)]), as.matrix(t[, c(2:8)]), abs(as.matrix(t[, c(2:8)])),
+            as.matrix(t_scores[, c(2:8)]),
+            along = 3, new.names = list(unname(unlist(a[1])), NULL, c("anna", "grace", "rachel", "autograder",
+                                                                      "truth", "positive_truth", "true_scores")))
+
+perc_error <- NULL
+perc_error["autograder"] <- round(sum(aa[ , , 6])/(dim(aa[ , , 6])[1]*dim(aa[ , , 6])[2])*100, 2)
+
+#perfect <- round(apply(aa[1:50, 1:26, c(4,6)], c(1,2), prod), 2)
+#write.csv(perfect, file = "exact_scores.csv")
+
+raters_err <- abind(apply(aa[, , c(1,7)], c(1,2), diff), apply(aa[, , c(2,7)], c(1,2), diff), 
+                    apply(aa[, , c(3,7)], c(1,2), diff), apply(aa[, , c(4,7)], c(1,2), diff),
+                    along = 3, new.names = list(unname(unlist(a[1])), NULL, c("anna", "grace", "rachel", "autograder")))
+
+
+
+points_error <- NULL
+perc_error["anna"] <- round(sum(raters_err[ , , c("anna")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["anna"] <- sum(abs(raters_err[ , , c("anna")]))
+perc_error["grace"] <- round(sum(raters_err[ , , c("grace")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["grace"] <- sum(abs(raters_err[ , , c("grace")]))
+perc_error["rachel"] <- round(sum(raters_err[ , , c("rachel")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["rachel"] <- sum(abs(raters_err[ , , c("rachel")]))
+perc_error["autograder"] <- round(sum(raters_err[ , , c("autograder")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["autograder"] <- sum(abs(raters_err[ , , c("autograder")]))
+
+tot_perc_error["anna"] <- sum(tot_perc_error["anna"], raters_err[ , , c("anna")] == 0)
+tot_points_error["anna"] <- sum(tot_points_error["anna"], abs(raters_err[ , , c("anna")]))
+tot_perc_error["grace"] <- sum(tot_perc_error["grace"], raters_err[ , , c("grace")] == 0)
+tot_points_error["grace"] <- sum(tot_points_error["grace"], abs(raters_err[ , , c("grace")]))
+tot_perc_error["rachel"] <- sum(tot_perc_error["rachel"], raters_err[ , , c("rachel")] == 0)
+tot_points_error["rachel"] <- sum(tot_points_error["rachel"], abs(raters_err[ , , c("rachel")]))
+tot_perc_error["autograder"] <- sum(tot_perc_error["autograder"], raters_err[ , , c("autograder")] == 0)
+tot_points_error["autograder"] <- sum(tot_points_error["autograder"], abs(raters_err[ , , c("autograder")]))
+
+tot_dim <- sum(tot_dim, dim(raters_err)[1]*dim(raters_err)[2])
+
+## ----percentage error 2--------------------------------------------------
+kable(perc_error, caption = "Percentage correct")
+
+## ----points error 2------------------------------------------------------
+kable(points_error, caption = "Total score error")
+
+kable(points_error/(sum(ag$points)*dim(raters_err)[2])*100, caption = "Percentage score error")
+
+
+## ----include=FALSE-------------------------------------------------------
+
+a <- read.csv("data/anna_exam3.csv", stringsAsFactors = FALSE)#exam1
+g <- read.csv("data/grace_exam3.csv", stringsAsFactors = FALSE)
+r <- read.csv("data/rachel_exam3.csv", stringsAsFactors = FALSE)
+ag <- read.csv("data/autog_exam3.csv", stringsAsFactors = FALSE)
+t <- read.csv("data/truth_exam3.csv", stringsAsFactors= FALSE)#truth exam 1 
+names(t)[1] <- "task"
+t_scores <- read.csv("data/truth_exam3.csv", stringsAsFactors = FALSE)
+names(t_scores)[1] <- "task"
+
+
+## ----reliability_validity 3, include=FALSE-------------------------------
+colnames(a) <- tolower(colnames(a))
+colnames(g) <- tolower(colnames(g))
+colnames(r) <- tolower(colnames(r))
+colnames(ag) <- tolower(colnames(ag))
+colnames(t) <- tolower(colnames(t))
+colnames(t_scores) <- tolower(colnames(t_scores))
+
+
+identical(colnames(a), colnames(g))
+identical(ag[1], g[1])
+
+
+col_retain <- intersect(colnames(a), colnames(ag))
+a <- a[2:23, col_retain]
+g <- g[2:23, col_retain]
+r <- r[2:23, col_retain]
+
+identical(colnames(a), colnames(ag))
+colnames(ag)
+ag <- ag[2:23, col_retain]
+identical(colnames(a), colnames(ag))
+colnames(ag)
+
+identical(colnames(a), colnames(t))
+identical(ag[1], t[1])
+t <- t[2:23, col_retain]
+identical(colnames(a), colnames(t))
+
+identical(colnames(a), colnames(t_scores))
+identical(ag[1], t_scores[1])
+t_scores <- t_scores[2:23, col_retain]
+identical(colnames(a), colnames(t_scores))
+
+aa <- abind(as.matrix(a[, c(3:10)]), as.matrix(g[, c(3:10)]), as.matrix(r[, c(3:10)]),
+            as.matrix(ag[, c(3:10)]), as.matrix(t[, c(3:10)]), abs(as.matrix(t[, c(3:10)])),
+            as.matrix(t_scores[, c(3:10)]),
+            along = 3, new.names = list(unname(unlist(a[1])), NULL, c("anna", "grace", "rachel", "autograder",
+                                                                      "truth", "positive_truth", "true_scores")))
+
+perc_error <- NULL
+perc_error["autograder"] <- round(sum(aa[ , , 6])/(dim(aa[ , , 6])[1]*dim(aa[ , , 6])[2])*100, 2)
+
+#perfect <- round(apply(aa[1:50, 1:26, c(4,6)], c(1,2), prod), 2)
+#write.csv(perfect, file = "exact_scores.csv")
+
+raters_err <- abind(apply(aa[, , c(1,7)], c(1,2), diff), apply(aa[, , c(2,7)], c(1,2), diff), 
+                    apply(aa[, , c(3,7)], c(1,2), diff), apply(aa[, , c(4,7)], c(1,2), diff),
+                    along = 3, new.names = list(unname(unlist(a[1])), NULL, c("anna", "grace", "rachel", "autograder")))
+
+
+
+points_error <- NULL
+perc_error["anna"] <- round(sum(raters_err[ , , c("anna")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["anna"] <- sum(abs(raters_err[ , , c("anna")]))
+perc_error["grace"] <- round(sum(raters_err[ , , c("grace")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["grace"] <- sum(abs(raters_err[ , , c("grace")]))
+perc_error["rachel"] <- round(sum(raters_err[ , , c("rachel")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["rachel"] <- sum(abs(raters_err[ , , c("rachel")]))
+perc_error["autograder"] <- round(sum(raters_err[ , , c("autograder")] == 0)/(dim(raters_err)[1]*dim(raters_err)[2])*100, 2)
+points_error["autograder"] <- sum(abs(raters_err[ , , c("autograder")]))
+
+tot_perc_error["anna"] <- sum(tot_perc_error["anna"], raters_err[ , , c("anna")] == 0)
+tot_points_error["anna"] <- sum(tot_points_error["anna"], abs(raters_err[ , , c("anna")]))
+tot_perc_error["grace"] <- sum(tot_perc_error["grace"], raters_err[ , , c("grace")] == 0)
+tot_points_error["grace"] <- sum(tot_points_error["grace"], abs(raters_err[ , , c("grace")]))
+tot_perc_error["rachel"] <- sum(tot_perc_error["rachel"], raters_err[ , , c("rachel")] == 0)
+tot_points_error["rachel"] <- sum(tot_points_error["rachel"], abs(raters_err[ , , c("rachel")]))
+tot_perc_error["autograder"] <- sum(tot_perc_error["autograder"], raters_err[ , , c("autograder")] == 0)
+tot_points_error["autograder"] <- sum(tot_points_error["autograder"], abs(raters_err[ , , c("autograder")]))
+
+tot_dim <- sum(tot_dim, dim(raters_err)[1]*dim(raters_err)[2])
+
+
+## ----percentage error 3--------------------------------------------------
+kable(perc_error, caption = "Percentage correct")
+
+## ----points error 3------------------------------------------------------
+kable(points_error, caption = "Total score error")
+
+kable(points_error/(sum(ag$points)*dim(raters_err)[2])*100, caption = "Percentage score error")
+
+perc_error <- NULL
+points_error <- NULL
+perc_error["anna"] <- round(tot_perc_error["anna"]/tot_dim*100, 2)
+points_error["anna"] <- tot_points_error["anna"]
+perc_error["grace"] <- round(tot_perc_error["grace"]/tot_dim*100, 2)
+points_error["grace"] <- tot_points_error["grace"]
+perc_error["rachel"] <- round(tot_perc_error["rachel"]/tot_dim*100, 2)
+points_error["rachel"] <- tot_points_error["rachel"]
+perc_error["autograder"] <- round(tot_perc_error["autograder"]/tot_dim*100, 2)
+points_error["autograder"] <- tot_points_error["autograder"]
+
+## ----percentage error tot--------------------------------------------------
+kable(perc_error, caption = "Percentage correct")
+
+## ----points error tot------------------------------------------------------
+kable(points_error, caption = "Total score error")
+
+kable(points_error/(sum(ag$points)*dim(raters_err)[2])*100, caption = "Percentage score error")
